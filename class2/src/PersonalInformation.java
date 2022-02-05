337ef87374b61bb2f40a99e8a45b500d2141e1e2
Ian Luchini <iancarloluchini@gmail.com>
2022-02-04 19:04:58 -0500

    completed class word

--- class2/src/PersonalInformation.java
+++ class2/src/PersonalInformation.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class PersonalInformation {
     public static void main(String[] args) {
-        System.out.println("Cyrus Meherji\n" +
-                           "123 Main Street\n");
+        Scanner kb = new Scanner(System.in);
+    
+        System.out.print("Enter name: ");
+        String name = kb.nextLine();
+
+        System.out.print("Enter college major: ");
+        String collegeMajor = kb.nextLine();
+
+        System.out.print("Enter address: ");
+        String address = kb.nextLine();
+
+        System.out.print("Enter city: ");
+        String city = kb.nextLine();
+
+        System.out.print("Enter state: ");
+        String state = kb.nextLine();
+
+        System.out.print("Enter zip: ");
+        int zip = kb.nextInt();        
+
+        System.out.print("Enter telephone: ");
+        int telephoneNumber = kb.nextInt();
+
+
+        kb.close();
+
+        System.out.println("\n\nName: " + name + 
+                        "\nCollege Major: " + collegeMajor +
+                        "\nAddress: " + address + 
+                        "\nCity: " + city +
+                        "\nState: " + state +
+                        "\nZip: " + zip +
+                        "\nTelephone: " + telephoneNumber);
     }
 }
